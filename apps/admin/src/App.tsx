6c4c602b33da93e72473551c705613c6d76bd014
bouajilaprog <bouajilamedyessine@gmail.com>
2026-05-27 17:59:25 +0100

    feat(admin:user): create user hook

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -1,10 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
+import LoginPage from './pages/login/loginPage'
+import { UserProvider } from './hooks/useUser'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<div className="p-8 text-center">Admin</div>} />
-    </Routes>
+    <UserProvider >
+      <Routes>
+        <Route path="/" element={<div className="p-8 text-center">Admin</div>} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </UserProvider>
   )
 }
 
