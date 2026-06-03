767d755100a5ab87fb8d34520e2a0cf4bce5bfd8
bouajilaprog <bouajilamedyessine@gmail.com>
2026-06-03 23:31:35 +0100

    feat(web): add Inter font and update typography

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/500.css'
+import '@fontsource/inter/600.css'
+import '@fontsource/inter/700.css'
 import './index.css'
 import App from './App.tsx'
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HashRouter>
